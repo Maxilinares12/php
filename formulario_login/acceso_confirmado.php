35a273ffdcb2c5eb2bfecad9c23b16b6966a68d5
Maxilinares12 <maxilinares11@gmail.com>
2022-07-17 22:06:35 -0300

    cambios

---
+++ formulario_login/acceso_confirmado.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+
+</head>
+<body>
+    <main class="container">
+        <div class="row">
+            <div class="col-12 pt-5">
+                <h1>Bienvenid@ al sistema</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 pt-3">
+                <div class="alert alert-primary" role="alert">
+                 Lorem ipsum dolor sit amet consectetur adipisicing elit. A distinctio sed magni atque libero eveniet aliquam facere beatae reiciendis laborum repellendus itaque porro dolorum voluptas labore, numquam velit delectus optio!
+                </div>
+            </div>
+        </div>
+            <div class="row">
+                <div class="col-12 my-5">
+                    <button type="submit" class="btn btn-primary">Salir</button>
+                </div>
+        </div>
+    </main>
+    
+</body>
+</html>
---
+++ formulario_login/index.php
@@ -0,0 +1,46 @@
+
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+</head>
+<body>
+    <main class="container">
+        <div class="row">
+            <div class="col-12 text-center pt-5">
+                <h1>Formulario </h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-6">
+                <form action="" method="post">
+                    <div class="pb-3">
+                        <label for="txtUsuario">Usuario</label>
+                        <input type="text" name="txtUsuario" id="txtUsuario" required class="form-control">
+                    </div>
+                    <div>
+                        <label for="txtClave">Clave</label>
+                        <input type="password" name="txtClave" id="txtClave" required class="form-control">
+                    </div>
+                    <div>
+                        <button class="btn btn-primary mt-3" name="btnEnviar" type="submit" id="btnEnviar">Enviar</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+
+    </main>
+</body>
+</html>
